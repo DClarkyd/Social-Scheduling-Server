c5abd656aef3107d5db162591f55861c8cc7e38a
Chase Fegenbush <chase.fegenbush22@gmail.com>
2018-09-14 14:00:54 -0400

    added json ignore to interest

--- Project2Spring/src/main/java/com/revature/model/Interests.java
+++ Project2Spring/src/main/java/com/revature/model/Interests.java
@@ -7,6 +7,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
 @Entity
 @Table(name = "interests")
 
