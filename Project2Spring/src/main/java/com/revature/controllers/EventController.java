5a1766602bef2867c5c423dc9a8c6da7e1e7e6b4
Josue Rodriguez <jrod_1729@hotmail.com>
2018-09-19 19:54:00 -0400

    ...

--- Project2Spring/src/main/java/com/revature/controllers/EventController.java
+++ Project2Spring/src/main/java/com/revature/controllers/EventController.java
@@ -3,21 +3,18 @@ package com.revature.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.revature.model.AppUser;
 import com.revature.model.Event;
 import com.revature.model.Interests;
 import com.revature.services.EventServices;
@@ -52,36 +49,12 @@ public class EventController {
 		Event event = es.findOne(id);
 		return event;
 	}
-
-<<<<<<< HEAD
-	// Find Event by Author ID
-	@GetMapping("{id}/author")
-	public List<Event> eventAuthor(@PathVariable int id) {
-		Event event = es.findOne(id);
-		return event.getEvents();
-=======
-	//find all events
-		@GetMapping
-		public List<Event> findAll() {
-			System.out.println("finding all events");
-			List<Event> events = new ArrayList<>();
-			return es.findAll();
-		}
 		
-		//find event by id
-		@CrossOrigin
-		@GetMapping("{id}")
-		public Event findById(@PathVariable int id) {
-			Event event = es.findOne(id);
-			return event;
-		}
-	
 	//Find Event by Author ID
 	@GetMapping("author/{id}")
 	public List<Event> eventAuthor(@PathVariable int id){
 		List<Event> event = es.findByAuthorId(id);
 		return event;
->>>>>>> b2667921144acc1fc3ad62115548d4a474824e30
 	}
 
 	@PostMapping
