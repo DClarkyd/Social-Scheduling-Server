1ddc9ecf32a3f6856d59e759a9896a29278ef8a2
Chase Fegenbush <chase.fegenbush22@gmail.com>
2018-09-19 20:24:33 -0400

    ...

--- Project2Spring/src/main/java/com/revature/controllers/EventController.java
+++ Project2Spring/src/main/java/com/revature/controllers/EventController.java
@@ -41,13 +41,7 @@ public class EventController {
 		Event event = es.findOne(id);
 		return event;
 	}
-<<<<<<< HEAD
-		
-=======
 
-<<<<<<< HEAD
-	
-=======
 	//find all events
 		@GetMapping
 		public List<Event> findAll() {
@@ -55,21 +49,13 @@ public class EventController {
 			List<Event> events = new ArrayList<>();
 			return es.findAll();
 		}
->>>>>>> 8358c4fe9aa6337eb58b37a7f407bc7ff9e6fcf0
-	
->>>>>>> 16ce44025a1b552fb02192bf902115e090fc61c1
+
 	//Find Event by Author ID
 	@GetMapping("author/{id}")
 	public List<Event> eventAuthor(@PathVariable int id){
 		List<Event> event = es.findByAuthorId(id);
 		return event;
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 
-=======
->>>>>>> 8358c4fe9aa6337eb58b37a7f407bc7ff9e6fcf0
->>>>>>> 16ce44025a1b552fb02192bf902115e090fc61c1
 	}
 
 	@PostMapping
